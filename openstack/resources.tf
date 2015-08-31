8e7dc2c9432918b75f1dae4263ff6120082ddcb2
Jan Alexander <jan@alexander.name>
2015-08-31 12:39:57 -0700

    Fixing typo in the variable name.

--- openstack/resources.tf
+++ openstack/resources.tf
@@ -83,7 +83,7 @@ resource "template_file" "cloud-init-master" {
     vars {
         dns_fallback = "${var.dns_fallback}"
         cluster_name = "${var.dcos_cluster_name}"
-        repository_url = "${var.dcos_respository_url}"
+        repository_url = "${var.dcos_repository_url}"
     }
 }
 
@@ -157,7 +157,7 @@ resource "template_file" "cloud-init-slave" {
 
     vars {
         dns_fallback = "${var.dns_fallback}"
-        repository_url = "${var.dcos_respository_url}"
+        repository_url = "${var.dcos_repository_url}"
     }
 }
 
